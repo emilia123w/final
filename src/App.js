1fdfe4e7a8f3ac1cb637baad4b641344f21eac99
Emilia Walczak <emilia.walczak.87@wp.pl>
2018-08-06 11:18:04 +0200

    add click

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import './style.css'
+import './style.css';
+import ReactDOM from 'react-dom';
 
 import Map from './Map.js';
 
@@ -8,24 +9,33 @@ import Map from './Map.js';
 
 
 class App extends Component {
-
+  openMenu=function(){
+    if(document.getElementById("navBar").style.visibility = "hidden"){
+    document.getElementById("navBar").style.visibility = "visible";
+    }
+    else{
+      document.getElementById("navBar").style.visibility = "hidden";
+    }
+}
 
 
   render(){
 
 
-
     return (
       <div className="app">
+      <div className="burgerMenu" onClick={this.openMenu}>
+      Menu
+      </div>
       <h2>ŚWIĘTOKRZYSKIE TRIP</h2>
       <Map/>
 
       </div>
 
+
     )
 
 }
-
 
 }
 export default App;
